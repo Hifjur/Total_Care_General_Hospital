ac8ff700cc32f44f3f038b9c67bcb75da3739d15
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-10-18 04:01:06 +0600

    first commit

---
+++ src/Pages/Authentication/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQ72Fls1Mp27e_vWa7kKFC7zdqs8Hya-w",
+    authDomain: "total-care-general-hospital.firebaseapp.com",
+    projectId: "total-care-general-hospital",
+    storageBucket: "total-care-general-hospital.appspot.com",
+    messagingSenderId: "336727297451",
+    appId: "1:336727297451:web:2aebe95c14cc95d48d96ee"
+  };
+
+  export default firebaseConfig;
