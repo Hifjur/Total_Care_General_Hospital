c9bb497b15032313bc855c629df467f4342fa4ee
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-28 00:24:53 +0600

    service booking confirmation page added

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -8,19 +8,28 @@ import { useParams } from 'react-router';
 const Appointment = (props) => {
     const [service, setService] = useState([]);
     useEffect(() => {
-        fetch('./services.json')
-        .then(res => res.json())
-        .then(data => setService(data));
-    },[])
+        fetch('https://raw.githubusercontent.com/Hifjur/test/main/services.json')
+            .then(res => res.json())
+            .then(data => setService(data));
+    }, [])
     console.log(service);
     const { appointmentId } = useParams();
-    //  const a= useContext(ServiceContext);
-     //console.log(a);
+    const bookingInfo = service[appointmentId - 1];
+    console.log(bookingInfo);
     return (
         <div>
-            <h1>id: {appointmentId}</h1>
-            
-            
+        { bookingInfo &&   <div className="grid justify-items-center p-2 border-4 m-3 filter drop-shadow-2xl rounded ">
+                <img className="rounded border-4 border-blue-200" src={bookingInfo.img} alt="" />
+                <div className="pb-3 pt-3">
+                    <h1 className="text-xl font-black text-blue-600">{bookingInfo.name}</h1>
+                    <p>{bookingInfo.description}</p>
+                    <p>Fee: ${bookingInfo.fees}</p>
+
+                    <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 mt-3 rounded inline-flex items-center ">Confirm</button>
+                </div>
+            </div>}
+
+
         </div>
     );
 
